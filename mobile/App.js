d63c81b122e1b73781fad3348e325bdec867ea43
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-08-10 13:41:49 -0300

    Estilizando pagina de Hello World

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text} >Hello World </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex', //Todos os componentes por default ja são flex
+    flexDirection: 'column', //O flexDirection por default ja é column
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#fff',
+  }
+})
